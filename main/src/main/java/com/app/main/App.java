25803edbdf1318bcafec5d81d0c94bd4769042d2
Paweł <pawelbicki@gmail.com>
2019-06-24 12:53:48 +0200

    v5 - App class has been changed / full working project

--- main/src/main/java/com/app/main/App.java
+++ main/src/main/java/com/app/main/App.java
@@ -2,9 +2,14 @@ package com.app.main;
 
 import com.app.service.MenuService;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        final String JSON_FILE_FOLDER = "C:\\Programowanie\\CarsManagementSystem2\\main\\src\\main\\resources\\";
+        System.out.println("Insert the path of the cars.json file please");
+        Scanner sc = new Scanner(System.in);
+        String m = sc.nextLine();
+        final String JSON_FILE_FOLDER = m;
         final String jsonFilename = JSON_FILE_FOLDER + "cars.json";
         MenuService menuService = new MenuService(jsonFilename);
         menuService.mainMenu();
